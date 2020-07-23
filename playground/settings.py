f36acacaf6df8afc1726f22ac1e28515ed98d555
Carlos Santiago <csantiago2706@gmail.com>
2020-07-23 15:14:06 -0600

    Crear app pages y agregar templates html

--- playground/settings.py
+++ playground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['138.68.6.230', '127.0.0.1','localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'playground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
